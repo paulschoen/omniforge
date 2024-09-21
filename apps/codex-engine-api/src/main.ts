288b6a16a86638ce548fb74e43b437970671d0ef
Paul Schoen <paulschoen@rivian.com>
2024-09-21 18:21:04 -0500

    updating max field size

--- apps/codex-engine-api/src/main.ts
+++ apps/codex-engine-api/src/main.ts
@@ -9,12 +9,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { graphqlUploadExpress } from 'graphql-upload-ts';
 
+const FIVE_MEGABYTES = 5 * 1024 * 1024;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
-  app.use(graphqlUploadExpress({ maxFileSize: 5 * 1000000, maxFiles: 5 }));
+  app.use(
+    graphqlUploadExpress({
+      maxFileSize: FIVE_MEGABYTES,
+      maxFieldSize: FIVE_MEGABYTES,
+      maxFiles: 1,
+    })
+  );
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
